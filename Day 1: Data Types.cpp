abd6115705c6be5e0a5084b54ef75b7827a11998
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2022-10-23 22:38:35 +0200

    Create Day 1: Data Types.cpp

---
+++ Day 1: Data Types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+int n;
+double x;
+string m;
+cin >>n>>x;
+cin.ignore();
+ getline(cin,m);
+n+=i;
+x+=d;
+
+cout<< n<<"\n"<<fixed << setprecision(1)<<x<<"\n"<<s<<m;
+    return 0;
+}
