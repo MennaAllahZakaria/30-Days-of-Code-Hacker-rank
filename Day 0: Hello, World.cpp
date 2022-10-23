483b28efcac93628ea99ddf48e1eccd0e0663b46
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2022-10-23 22:36:46 +0200

    Create Day 0: Hello, World.cpp

---
+++ Day 0: Hello, World.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+  
+    cout << "Hello, World." << endl;
+    
+    return 0;
+}
