da305ae6072f89b2cb070af58f4628c9a3fb5d97
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2022-10-23 22:43:53 +0200

    Create Day 2: Operators.cpp

---
+++ Day 2: Operators.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+    
+double tip=meal_cost*tip_percent/100;
+    double tax=meal_cost*tax_percent/100;
+   cout<< (int) round(tax+tip+meal_cost);
+}
+
+int main()
+{
+  
+    double meal_cost ;
+    int tip_percent , tax_percent ;
+    cin >> meal_cost>>tip_percent>>tax_percent;
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
+
