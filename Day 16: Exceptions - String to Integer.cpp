71855a175824aee960d0d37ba66ef746cbed2714
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2022-10-25 00:05:06 +0200

    Create Day 16: Exceptions - String to Integer.cpp

---
+++ Day 16: Exceptions - String to Integer.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    string S;
+    getline(cin, S);
+try {
+cout<< stoi(S);
+} catch (exception e) {
+cout<<"Bad String";
+}
+    return 0;
+}
