5d1d375034a39a4f6f5be72c110352d7f4832414
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2022-10-25 00:02:35 +0200

    Create Day 14: Scope.cpp

---
+++ Day 14: Scope.cpp
@@ -0,0 +1,55 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+     Difference(vector<int>a){
+         elements=a;
+     }
+     void computeDifference(){
+         
+         maximumDifference=INT32_MIN;
+         
+         for ( int i=0;i<elements.size()-1;i++){
+             
+             for ( int j=i+1;j<elements.size();j++){
+                 
+                 maximumDifference= max(maximumDifference,abs(elements[i]-elements[j]));
+             }
+         }
+         
+     }
+
+
+};
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
