6fe04570d4ba32f5bad23a1221edb10c48743099
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2022-10-26 22:25:08 +0200

    Create Day 29: Bitwise AND.cpp

---
+++ Day 29: Bitwise AND.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int bitwiseAnd(int N, int K) {
+int ma=INT_MIN;
+for ( int i=1;i<=N;i++){
+   for ( int j=i+1;j<=N;j++){
+       if ((i&j)<K) ma=max(ma,(i&j));   
+   }
+}
+
+return ma;
+
+}
+
+int main()
+{
+int t;
+cin >>t;
+while ( t--){
+    
+    int n,k;
+    cin >>n>>k;
+    cout<<bitwiseAnd(n,k)<<"\n";
+    
+    
+}
+    return 0;
+}
