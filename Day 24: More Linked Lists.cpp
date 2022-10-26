ea4e9e1d7d61975abb0cdc257a339bd32fd080c4
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2022-10-26 17:04:26 +0200

    Create Day 24: More Linked Lists.cpp

---
+++ Day 24: More Linked Lists.cpp
@@ -0,0 +1,88 @@
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+          Node* removeDuplicates(Node *head)
+          {
+              Node* newhead=head;
+               while ( newhead->next){
+                   
+               
+                    if (newhead->data==newhead->next->data ){
+                        newhead->next=newhead->next->next;
+                   
+                    }
+                  else newhead=newhead->next;
+                   
+               }
+             return head;
+         
+          }
+
+
+
+          Node* insert(Node *head,int data)
+          {
+               Node* p=new Node(data);
+               if(head==NULL){
+                   head=p;  
+
+               }
+               else if(head->next==NULL){
+                   head->next=p;
+
+               }
+               else{
+                   Node *start=head;
+                   while(start->next!=NULL){
+                       start=start->next;
+                   }
+                   start->next=p;   
+
+               }
+                    return head;
+                
+            
+          }
+          void display(Node *head)
+          {
+                  Node *start=head;
+                    while(start)
+                    {
+                        cout<<start->data<<" ";
+                        start=start->next;
+                    }
+           }
+};
+			
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+    head=mylist.removeDuplicates(head);
+
+	mylist.display(head);
+		
+}
