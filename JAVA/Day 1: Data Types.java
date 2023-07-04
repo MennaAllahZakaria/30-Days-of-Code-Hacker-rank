674cee43bb9a694cb2fb908c484abb9cf8793e06
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-07-04 16:40:19 +0300

    Day 1: Data Types.java

---
+++ JAVA/Day 1: Data Types.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);
+ 
+int n= sc.nextInt();
+double x= sc.nextDouble();
+sc.nextLine();
+String m=sc.nextLine();
+
+System.out.println(n+4);
+System.out.println(x+4.0);
+System.out.println("HackerRank "+m);
+
+    }
+}
