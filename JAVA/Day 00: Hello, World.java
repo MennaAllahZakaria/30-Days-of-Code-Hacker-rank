960b7184c6885b3cc138911a96e782a0efb50fcb
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-07-04 16:42:51 +0300

    Day 00: Hello, World.java

---
+++ JAVA/Day 00: Hello, World.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);  
+         String name = sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(name);
+    }
+}
